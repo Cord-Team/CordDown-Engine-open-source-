353071d0b981db535689e8a3968f8c17afdcf47f
InPancakepoiDev <so9037211@gmail.com>
2025-10-30 17:37:53 -0600

    Hai
    Print 'hi!' multiple times to the console.

---
+++ #include <iostream>  int main() {   /hello.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+  std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+
+
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
+ std::cout << "hi!" << std::endl;
+
+ std::cout << "hi!" << std::endl;
